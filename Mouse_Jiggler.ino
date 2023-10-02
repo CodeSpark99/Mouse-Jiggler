669b74d37545d148edac5f20ae5b4dc914773208
CodeSpark99 <90392920+CodeSpark99@users.noreply.github.com>
2023-10-02 10:37:28 +0530

    Mouse_Jiggler.ino

--- Mouse_Jiggler.ino
+++ Mouse_Jiggler.ino
@@ -18,10 +18,10 @@ void loop()
  mouseReport.buttons = 0;
  mouseReport.x = 0;
  mouseReport.y = 0;
- int xr_val = random(-1024, 1024);
- int yr_val = random(-768, 768);
- mouseReport.x = map(xr_val, -1024, 1023, -10, 10);
- mouseReport.y = map(yr_val, -768, 767, -10, 10);
+ int xr_val = random(-1280, 1279);
+ int yr_val = random(-720, 719);
+ mouseReport.x = map(xr_val, -1280, 1279, -10, 10);
+ mouseReport.y = map(yr_val, -720, 719, -10, 10);
  for (ind=0; ind<20; ind++) {
 	Serial.write((uint8_t *)&mouseReport, 4);
 	Serial.write((uint8_t *)&nullReport, 4);
